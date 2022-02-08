e9d041f76e40ad2122124a4bdf895cefc5fbbc0d
liza-mae <liza.dayoub@elastic.co>
2022-02-08 09:31:41 -0700

    Cleanup

--- ci/cloud/runme.sh
+++
@@ -1,4 +0,0 @@
-
-export WORKSPACE=/home/liza/CHECKINS/estf-checkin-repo
-JOB=basicGrp1 ESTF_CLOUD_HOST=admin.staging.foundit.no ESTF_CLOUD_VERSION=8.1.0 ESTF_KIBANA_VERSION=main ./gradlew saas_setup 
-
